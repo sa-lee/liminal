9b63d6d11be251a3e4c69faddb696324da24bbdc
Stuart Lee <lee.s@wehi.edu.au>
2020-07-02 17:14:06 +1000

    add linked ui

--- R/tour-ui.R
+++ R/tour-ui.R
@@ -61,60 +61,25 @@ gadget_tour_ui <- function(axis = TRUE) {
 }
 
 
-limn_tour_ui <- function(view = "simple", nr) {
-  view <- match.arg(view, c("simple", "linked"))
-
-
-  # views always present
-  tview <- vegawidget::vegawidgetOutput("tourView")
-  aview <- vegawidget::vegawidgetOutput("axisView")
-
-  # control panel
-  title <- shiny::h4("liminal controls")
-  play <- shiny::actionButton("play", "Play", icon = shiny::icon("play"))
-
-  half_rng <- shiny::textOutput(outputId = "half_range")
-
-  if (view == "linked") {
-    brush_rdo <- shiny::radioButtons("brush_selector",
-                                     "Selection Type",
-                                     choices = c("linked",
-                                                 "neighbors",
-                                                 "distance"))
-    brush_fixed <- shiny::radioButtons("brush_logic",
-                                       "Selection Sequence",
-                                       choices = c("or", "and", "independent"))
-
-    brush_knn <- shiny::numericInput("brush_knn",
-                                     "Neighbors:",
-                                     value = 10,
-                                     min = 0,
-                                     max = nr)
-
-    bottom_row <- shiny::fluidRow(
-      shiny::column(4, aview),
-      shiny::column(6,
-                    shiny::fluidRow(
-                      shiny::column(3, title, play),
-                      shiny::column(3, brush_rdo),
-                      shiny::column(3, brush_fixed),
-                      shiny::column(3, brush_knn)
-                    )
+gadget_linked_ui <- function() {
+  miniPage(
+    gadget_tour_titlebar(),
+    miniTabstripPanel(
+      miniTabPanel("Controls", icon = icon("sliders"),
+                   miniContentPanel(
+                     numericInput("k", "Neighbors:",
+                                  value = 1, min = 1, max = 25),
+                     radioButtons("metric",
+                                  "Distance Metric",
+                                  selected = "euclidean",
+                                  choices = c("euclidean",
+                                              "cosine",
+                                              "manhattan"))                                  )
       ),
-      shiny::column(2, shiny::h5("Half Range"), half_rng)
+      miniTabPanel("Embed + Tour", icon = icon("map-o"),
+                   gadget_tour_main_panel(axis = FALSE),
+                   gadget_tour_controls()
+      )
     )
-  } else {
-    bottom_row <- shiny::fluidRow(
-      shiny::column(4, aview),
-      shiny::column(4, play),
-      shiny::column(4, shiny::h5("Half Range"), half_rng)
-    )
-  }
-
-  tview_ui <-   shiny::fluidRow(shiny::column(12, tview))
-
-  shiny::fluidPage(
-    tview_ui,
-    bottom_row
   )
 }
