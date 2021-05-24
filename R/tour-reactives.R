d48f680c86a391bc0ff68825afa4b54d6fa87735
sa-lee <lee.s@wehi.edu.au>
2021-05-24 11:25:50 +1000

    update tourr requirement

--- R/tour-reactives.R
+++ R/tour-reactives.R
@@ -16,12 +16,18 @@ rct_half_range <- function(rct_zoom, half_range) {
 rct_tour <- function(plan, tour_data, tour_path, selections, aps = 1, fps = 8) {
   current <- plan(0)
   shiny::reactive({
+    # required as of tourr 0.6.0
+    record <- dplyr::tibble(
+      basis = list(), index_val = numeric(),
+      info = character(), method = character(), alpha = numeric(),
+      tries = numeric(), loop = numeric()
+    )
+
     play <- selections[["do_tour"]]
 
     play <- current$step >= 0 && play
     if (play) {
       current <<- plan(aps / fps)
-      print(current)
       selections[["proj"]] <- current$proj
       shiny::invalidateLater(1000 * aps / fps)
     }
